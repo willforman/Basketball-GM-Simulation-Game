7f61c5d1e5700b158e190cef76b0dc8e78cdd248
willforman <wf8581@gmail.com>
2021-05-18 15:17:49 -0500

    Add background accent color

--- packages/client/src/@chakra-ui/gatsby-plugin/colors.js
+++ packages/client/src/@chakra-ui/gatsby-plugin/colors.js
@@ -1,8 +1,9 @@
 export const colors = {
   bball: {
     main: "#6B46C1",
-    mainLight: "#805AD5",
+    main_light: "#805AD5",
     background: "#1a202c",
     test: "#eb4034",
+    background_accent: "#011627",
   },
 };
