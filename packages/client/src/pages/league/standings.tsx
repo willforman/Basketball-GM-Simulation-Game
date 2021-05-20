9435e27b8e99df30939d21dc27f8d840e60b5ad2
willforman <wf8581@gmail.com>
2021-05-19 19:30:34 -0500

    Remove standings page and add playoffs

--- packages/client/src/pages/league/standings.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Layout from "../../components/layout";
-
-const Standings: React.FC = () => {
-  return <Layout>Standings</Layout>;
-};
-
-export default Standings;
