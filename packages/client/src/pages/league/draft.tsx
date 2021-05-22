7c6d7b907cfb43a6cd10548c724a1c6f44c4e92e
willforman <wf8581@gmail.com>
2021-05-22 13:25:46 -0500

    Placeholder if draft isn't generated yet

--- packages/client/src/pages/league/draft.tsx
+++ packages/client/src/pages/league/draft.tsx
@@ -3,6 +3,7 @@ import PlayerTable from "../../components/playerTable";
 import Layout from "../../components/layout";
 import { useLeague } from "../../context/league";
 import { navigate } from "gatsby";
+import { Text } from "@chakra-ui/react";
 
 const Draft: React.FC = () => {
   const { league } = useLeague();
@@ -15,6 +16,14 @@ const Draft: React.FC = () => {
 
   if (!league) {
     return <div></div>;
+  }
+
+  if (!league.draft) {
+    return (
+      <Layout>
+        <Text>No draft</Text>
+      </Layout>
+    );
   }
 
   return (
